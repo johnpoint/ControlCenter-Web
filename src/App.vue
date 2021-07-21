0f2d1a864d8742242f0c0bb931dbe1c9416a82b3
johnpoint <me@lvcshu.com>
2021-07-21 10:46:16 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <topMenu v-if="$route.path!='/Login' && $route.path!='/403'"/>
+    <topMenu v-if="$route.path != '/Login' && $route.path != '/403'" />
     <div id="app" class="main">
-      <router-view v-if="loaded" :key="viewkey" class="main-view"/>
+      <router-view v-if="loaded" :key="viewkey" class="main-view" />
     </div>
   </div>
 </template>
@@ -18,105 +18,116 @@
 }
 </style>
 <script>
-import topMenu from '@/components/topMenu'
+import topMenu from "@/components/topMenu";
 import config from "@/config";
 import router from "@/router";
 
 Date.prototype.format = function (fmt) {
   const o = {
-    "M+": this.getMonth() + 1,                 //月份
-    "d+": this.getDate(),                    //日
-    "h+": this.getHours(),                   //小时
-    "m+": this.getMinutes(),                 //分
-    "s+": this.getSeconds(),                 //秒
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "h+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
     "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-    "S": this.getMilliseconds()             //毫秒
+    S: this.getMilliseconds(), //毫秒
   };
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
   }
   for (const k in o) {
     if (new RegExp("(" + k + ")").test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length)
+      );
     }
   }
   return fmt;
-}
+};
 export default {
   components: {
-    topMenu
+    topMenu,
   },
   data() {
     return {
-      loaded: false
-    }
+      loaded: false,
+    };
   },
   mounted() {
-
     let lock = 0;
 
     function connWS(token, v) {
       if (lock == 1) {
-        return
+        return;
       }
       lock = 1;
-      let uri = 'ws://';
-      if (window.location.protocol === 'https:') {
-        uri = 'wss://';
+      let uri = "ws://";
+      if (window.location.protocol === "https:") {
+        uri = "wss://";
       }
       let addr;
-      addr = config.apiAddress.replace("http://", "").replace("https://", "")
-      v.$message.info('Connecting to API interface');
-      const connection = new WebSocket(uri + addr + "/api/v2")
+      addr = config.apiAddress.replace("http://", "").replace("https://", "");
+      v.$message.info("Connecting to API interface");
+      const connection = new WebSocket(uri + addr + "/api/v2");
       v.$store.state.ws = connection;
       v.$store.state.ws.onopen = () => {
         v.$store.state.ws.send(token);
         v.loaded = true;
         v.$message({
-          message: 'Connected~',
-          type: 'success'
+          message: "Connected~",
+          type: "success",
         });
         lock = 0;
-        delete v.$store.state.ws.onopen
-      }
+        delete v.$store.state.ws.onopen;
+      };
 
       v.$store.state.ws.onclose = function () {
-        console.log("close")
+        console.log("close");
         lock = 0;
         delete v.$store.state.ws.onclose;
-        setTimeout(connWS(token, v),5000)
-      }
+        setTimeout(connWS(token, v), 5000);
+      };
     }
 
     if (localStorage.getItem("isLogin") === "true") {
-      this.$store.commit('setjwt', localStorage.getItem("jwt"))
-      this.$store.commit('setStatus', true)
-      this.$http.get(config.apiAddress + "/web/UserInfo/Token", {
-        headers: {
-          'Authorization': "Bearer " + this.$store.state.jwt,
-          'Accept': 'application/json'
-        }
-      }).then(function (res) {
-        if (res.body.Code === 200) {
-          connWS(res.body.Info, this)
-        } else {
-          this.$notify({
-            title: 'Server Warning',
-            message: res.body.Info,
-            type: 'warning'
-          })
-        }
-      }, function () {
-        this.$notify({
-          title: 'Server Warning',
-          message: "登录会话可能已经过期，请尝试重新登录",
-          type: 'warning'
+      this.$store.commit("setjwt", localStorage.getItem("jwt"));
+      this.$store.commit("setStatus", true);
+      this.$http
+        .get(config.apiAddress + "/web/UserInfo/Token", {
+          headers: {
+            Authorization: "Bearer " + this.$store.state.jwt,
+            Accept: "application/json",
+          },
         })
-      })
+        .then(
+          function (res) {
+            if (res.body.Code === 200) {
+              connWS(res.body.Info, this);
+            } else {
+              this.$notify({
+                title: "Server Warning",
+                message: res.body.Info,
+                type: "warning",
+              });
+            }
+          },
+          function () {
+            this.$notify({
+              title: "Server Warning",
+              message: "登录会话可能已经过期，请尝试重新登录",
+              type: "warning",
+            });
+          }
+        );
+      next({ name: "/403" });
     } else {
-      this.loaded = true
-      router.push("/403")
+      this.loaded = true;
+      next({ name: "/403" });
     }
-  }
-}
+  },
+};
 </script>
